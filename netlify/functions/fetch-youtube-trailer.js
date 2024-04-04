506e24d28ac5b4713234bcea1066561a32dc094e
Evan Shaw <evanshaw34@gmail.com>
2024-04-04 13:05:55 -0700

    Add youtube fetching API hanlder (netlify funct)

--- netlify/functions/fetch-youtube-trailer.js
+++ netlify/functions/fetch-youtube-trailer.js
@@ -1,16 +1,25 @@
-export async function fetchYoutubeTrailer(gameName) {
-    try {
-      const response = await fetch(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&maxResults=1&q=${gameName} game trailer&key=${youtubeKey}`
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch youtube data");
-      }
-      const youtubeData = await response.json();
-      const videos = youtubeData.items;
-      // console.log(youtubeData);
-      return videos;
-    } catch (error) {
-      console.error("Error fetching youtube data");
+export async function handler(event) {
+  const gameName = event.queryStringParameters.gameName;
+
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&maxResults=1&q=${gameName} game trailer&key=${youtube_key}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch youtube data");
     }
+    const youtubeData = await response.json();
+    const videos = youtubeData.items;
+    // console.log(youtubeData);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(videos),
+    };
+  } catch (error) {
+    console.error("Error fetching youtube data", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "failed to fetch youtube data" }),
+    };
   }
+}
