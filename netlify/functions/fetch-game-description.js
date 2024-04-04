02c13e993dc34af5eba5651517f97de7cbe6f31e
Evan Shaw <evanshaw34@gmail.com>
2024-04-04 12:59:09 -0700

    Add game desc fetching API handler (netlify funct)

--- netlify/functions/fetch-game-description.js
+++ netlify/functions/fetch-game-description.js
@@ -1,12 +1,20 @@
-export async function getGameDescription(gameId) {
-    try {
-      const response = await fetch(`https://api.rawg.io/api/games/${gameId}${apiKey}`);
-      if (!response.ok) {
-        throw new Error("Failed to fetch game desc data");
-      }
-      const data = await response.json();
-      return data.description;
-    } catch (error) {
-      console.error("Error fetching game descriptions ERROR: ", error);
+export async function handler(event) {
+  const gameId = event.queryStringParameters.gameId;
+  try {
+    const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=${apiKey}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch game description data");
     }
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ description: data.description }),
+    };
+  } catch (error) {
+    console.error("Error fetching game descriptions ERROR: ", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch game description" }),
+    };
   }
+}
