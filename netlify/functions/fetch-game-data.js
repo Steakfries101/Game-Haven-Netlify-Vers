06bd8887650928914dd3674ea9b517d4b29ce09e
Evan Shaw <evanshaw34@gmail.com>
2024-04-04 12:57:56 -0700

    Add game data fetching from RAWG API handler

--- netlify/functions/fetch-game-data.js
+++ netlify/functions/fetch-game-data.js
@@ -1,14 +1,25 @@
-export async function fetchGameData(gameName) {
-    try {
-      const response = await fetch(`https://api.rawg.io/api/games${apiKey}&search=${gameName}&search_precise=true&ordering=name?parent_platforms=1`);
-      if (!response.ok) {
-        throw new Error("Failed to fetch game data");
-      }
-  
-      const data = await response.json();
-      const games = data.results;
-      return games;
-    } catch (error) {
-      console.error("Error fetching game data", error);
+export async function handler(event) {
+  const gameName = event.queryStringParameters.gameName;
+
+  try {
+    const response = await fetch(
+      `https://api.rawg.io/api/games?key=${apiKey}&search=${gameName}&search_precise=true&ordering=name&parent_platforms=1`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch game data");
     }
+
+    const data = await response.json();
+    const games = data.results;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(games),
+    };
+  } catch (error) {
+    console.error("Error fetching game data", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch game data" }),
+    };
   }
+}
