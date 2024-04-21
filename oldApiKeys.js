dc64fa5d2c0518176ae1454d5cceb533c3e78f10
Evan Shaw <evanshaw34@gmail.com>
2024-04-20 18:25:14 -0700

    Rename and regenerate API Keys

--- oldApiKeys.js
+++ oldApiKeys.js
@@ -1,4 +1,6 @@
+//These Keys are no longer valid for security reasons
+
 export const youtubeKey = "AIzaSyCsEU3Fe6wNACeFTvZQgKA46QnreQL12NI";
 export const apiKey = "?key=eb297cab936749b380d022a9e1f0c9f1";
 export const baseUrl = "https://api.rawg.io/api/games";
-// //RAWG API
+
